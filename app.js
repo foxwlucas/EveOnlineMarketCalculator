3ef86b539bdc525c50e6acdabc295015fbc05bf4
foxwlucas <foxwlucas55@gmail.com>
2022-02-10 20:27:29 -0600

    hard coded version of the correct functionality

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+var axios = require('axios');
+axios.get("https://esi.evetech.net/latest/markets/10000002/orders/?datasource=tranquility&order_type=all&page=1&type_id=34")
+    .then(function (response) {
+    var responseData = response.data;
+    // Sell orders
+    var sellOrders = response.data.filter(function (data) { return data.is_buy_order === false; });
+    var largestSellData = sellOrders.filter(function (x) { return x.price > 10000; });
+    var largestSellPrice = largestSellData[0].price;
+    // Buy orders
+    var buyOrders = responseData.filter(function (data) { return data.is_buy_order; });
+    var smallestBuyData = buyOrders.filter(function (x) { return x.price <= 0.01; });
+    var smallestBuyPrice = smallestBuyData[0].price;
+    // Margin
+    console.log("The margin on this item is: ".concat(largestSellPrice - smallestBuyPrice, "."));
+})["catch"](function (error) {
+    // handle error
+    console.log(error);
+})
+    .then(function () {
+    // always executed
+});
--- app.ts
+++ app.ts
@@ -1,1 +1,28 @@
-console.log("hello world");
+const axios = require('axios');
+
+axios.get("https://esi.evetech.net/latest/markets/10000002/orders/?datasource=tranquility&order_type=all&page=1&type_id=34")
+    .then(function (response) {
+        let responseData = response.data;
+
+        // Sell orders
+        let sellOrders = response.data.filter(data => data.is_buy_order === false);
+        let largestSellData = sellOrders.filter(x => x.price > 10000);
+        let largestSellPrice = largestSellData[0].price;
+
+
+        // Buy orders
+        let buyOrders = responseData.filter(data => data.is_buy_order);
+        let smallestBuyData = buyOrders.filter(x => x.price <= 0.01);
+        let smallestBuyPrice = smallestBuyData[0].price;
+
+
+        // Margin
+        console.log(`The margin on this item is: ${largestSellPrice - smallestBuyPrice}.`)
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+    .then(function () {
+        // always executed
+    });
